78c2935118645fc6d02a679339980ac567bec26c
dakshesh252 <101445356+dakshesh252@users.noreply.github.com>
2022-07-02 03:50:42 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyBwldqZwNtyCpqfJYvcvDeHab2C0xtNSgA",
+    authDomain: "let-s-chat-web-app-3ebdd.firebaseapp.com",
+    databaseURL: "https://let-s-chat-web-app-3ebdd-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-web-app-3ebdd",
+    storageBucket: "let-s-chat-web-app-3ebdd.appspot.com",
+    messagingSenderId: "627999803616",
+    appId: "1:627999803616:web:d3eb34fc0fd6ab53f7e885"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
